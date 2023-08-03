067b6dcda267d49048de0b8f99881c532b11972e
Petr Vlasak <petr.vlasakk@gmail.com>
2023-08-03 12:31:42 +0200

    Initial Flask DB Migration created

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whiplash_database.db'
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "my_secret"
 
 db = SQLAlchemy(app)
+migrate=Migrate(app, db)
 
 
 def create_db():
